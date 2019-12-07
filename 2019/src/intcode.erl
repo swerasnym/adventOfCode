96925be2cd54f30219d1ec9521b33d779e755568
swerasnym <rasmus_nyman@hotmail.com>
2019-12-07 18:07:44 +0100

    Refactor Intcode

--- 2019/src/intcode.erl
+++ 2019/src/intcode.erl
@@ -11,7 +11,8 @@
 	 memory,
 	 input = [],
 	 output = [],
-	 outputpid = none
+	 outputpid = none,
+	 function = none
 	}).
 
 % Main api
@@ -32,168 +33,102 @@
 	 get_output/1,
 	 print/1]).
 
-%% Testing api
--export(
-   [
-    instruction/1,
-    parameters/1,
-    optcode/1,
-    call/1,
-    call/2,
-    get_addresses/3,
-    get_values/2,
-    mode/2
-   ]).
+
+-define(INSTRUCTIONS,
+	#{1 => {add, 3, fun add/1},
+	  2 => {multiply, 3, fun multiply/1},
+	  3 => {input, 1, fun input/1},
+	  4 => {output, 1, fun output/1},
+	  5 => {jump_if_true, 2, fun jump_if_true/1},
+	  6 => {jump_if_false, 2, fun jump_if_false/1},
+	  7 => {less_than, 3, fun less_than/1},
+	  8 => {equals, 3, fun equals/1},
+
+	  guess1 => {subtract, 3, fun subtract/1},
+	  guess2 => {divide, 3, fun divide/1},
+	  guess3 => {reminder, 3, fun reminder/1},
+	  guess4 => {jump, 3, fun jump/1},
+
+	  99 => {halt, 0, fun halt/1}}).
 
 
-instruction(1) ->
-    add;
-instruction(2) ->
-    multiply;
-instruction(3) ->
-    input;
-instruction(4) ->
-    output;
-instruction(5) ->
-    jump_if_true;
-instruction(6) ->
-    jump_if_false;
-instruction(7) ->
-    less_than;
-instruction(8) ->
-    equals;
-instruction(99) ->
-    halt.
-
-parameters(add) ->
-    3;
-parameters(multiply) ->
-    3;
-parameters(input) ->
-    1;
-parameters(output) ->
-    1;
-parameters(jump_if_true) ->
-    2;
-parameters(jump_if_false) ->
-    2;
-parameters(less_than) ->
-    3;
-parameters(equals) ->
-    3;
-parameters(subtract) -> %% Guess
-    3;
-parameters(divide) -> %% Guess
-    3;
-parameters(reminder) -> %% Guess
-    3;
-parameters(jump) -> %% Guess
-    1;
-parameters(halt) ->
-    0.
+-define(vva(V1, V2, A), #state{values = [V1, V2, _], addresses = [_,_,A]}).
 
 
-optcode(add)->
-    1;
-optcode(multiply) ->
-    2;
-optcode(input) ->
-    3;
-optcode(output) ->
-    4;
-optcode(jump_if_true) ->
-    5;
-optcode(jump_if_false) ->
-    6;
-optcode(less_than) ->
-    7;
-optcode(equals) ->
-    8;
-optcode(halt) ->
-    99.
+call(#state{function = Function} = State) ->
+    Function(State).
+
+add(?vva(Term1,Term2, To) = State) ->
+    set(Term1 + Term2, To, State).
+
+multiply(?vva(Factor1, Factor2, To) = State) ->
+    set(Factor1 * Factor2, To, State).
 
 
-call(#state{instruction = Instruction} = State) ->
-    call(Instruction, State).
-
-call(add, #state{values = [Term1, Term2, _], addresses = [_,_,To]} = State) ->
-    set(Term1 + Term2, To, State);
-
-call(multiply, #state{values = [Factor1, Factor2, _], addresses = [_,_,To]} = State) ->
-    set(Factor1 * Factor2, To, State);
-
-
-call(input, #state{addresses = [To], input = [] } = State0) ->
+input(#state{addresses = [To], input = [] } = State0) ->
     receive
 	[Value|Rest] ->
 	    State = set(Value, To , State0),
 	    State#state{input=Rest}
     end;
-
-call(input, #state{addresses = [To], input = [Value|Rest] } = State0) ->
+input(#state{addresses = [To], input = [Value|Rest] } = State0) ->
     State = set(Value, To , State0),
-    State#state{input=Rest};
+    State#state{input=Rest}.
 
 
-call(output, #state{values = [Value], output = Output, outputpid = none} = State) ->
+output(#state{values = [Value], output = Output, outputpid = none} = State) ->
     State#state{output=[Value | Output]};
-
-call(output, #state{values = [Value], output = Output, outputpid = Pid } = State) ->
+output(#state{values = [Value], output = Output, outputpid = Pid } = State) ->
     Pid ! [Value],
-    State#state{output=[Value | Output]};
+    State#state{output=[Value | Output]}.
 
-
-
-call(jump_if_true, #state{values = [Value, To]} = State ) ->
+jump_if_true(#state{values = [Value, To]} = State ) ->
     case Value of
 	0 ->
 	    State;
 	Value ->
 	    State#state{next_ip = To}
-    end;
+    end.
 
-call(jump_if_false, #state{values = [Value, To]} = State ) ->
+jump_if_false(#state{values = [Value, To]} = State ) ->
     case Value of
 	0 ->
 	    State#state{next_ip = To};
 	Value ->
 	    State
-    end;
+    end.
 
-
-
-call(less_than, #state{values = [Term1, Term2, _], addresses = [_,_,To]} = State) when Term1 < Term2 ->
+less_than(?vva(Term1,Term2, To) = State) when Term1 < Term2 ->
     set(1, To, State);
+less_than(?vva(_, _, To) = State) ->
+    set(0, To, State).
 
-call(less_than, #state{addresses = [_,_,To]} = State) ->
-    set(0, To, State);
-
-call(equals, #state{values = [Term, Term, _], addresses = [_,_,To]} = State) ->
+equals(?vva(Term,Term, To) = State) ->
     set(1, To, State);
-call(equals, #state{addresses = [_,_,To]} = State) ->
-    set(0, To, State);
+equals(?vva(_, _, To) = State) ->
+    set(0, To, State).
+
+subtract(?vva(Term1, Term2, To) = State) ->
+    set(Term1 - Term2, To, State).
 
 
+divide(?vva(Nominator, Denominator, To) = State) ->
+    set(Nominator div  Denominator, To, State).
 
-call(subtract, #state{values = [Term1, Term2, _], addresses = [_,_,To]} = State) ->
-    set(Term1 - Term2, To, State);
+reminder(?vva(Nominator, Denominator, To) = State) ->
+    set(Nominator rem  Denominator, To, State).
 
+jump(#state{values = [To]} = State)->
+    State#state{next_ip = To}.
 
-
-call(divide, #state{values = [Nominator, Denominator, _], addresses = [_,_,To]} = State) ->
-    set(Nominator div  Denominator, To, State);
-
-call(reminder, #state{values = [Nominator, Denominator, _], addresses = [_,_,To]} = State) ->
-    set(Nominator rem  Denominator, To, State);
-
-
-call(jump, #state{addresses = [To]} = State)->
-    State#state{next_ip = To};
-
-
-
-call(halt, _) ->
+halt(_) ->
     halt.
+
+
+
+
+
+
 
 
 print(#state{
@@ -315,8 +250,7 @@ get_values(Addresses, State) ->
 step_ip(#state{next_ip = Ip} = State) ->
 
     Intcode = get(Ip, State),
-    Instruction = instruction(Intcode rem 100),
-    Parameters = parameters(Instruction),
+    {Instruction, Parameters, Function} = maps:get(Intcode rem 100, ?INSTRUCTIONS),
     Modes = mode(Intcode div 100, Parameters),
     Addresses = get_addresses(Ip, Modes, State),
     Values = get_values(Addresses, State),
@@ -326,7 +260,8 @@ step_ip(#state{next_ip = Ip} = State) ->
 		instruction = Instruction,
 		addresses = Addresses,
 		values = Values,
-		mode = Modes}.
+		mode = Modes,
+		function = Function}.
 
 
 
