ddc206ce93477faea5c0dbd700486e3dc59b939f
swerasnym <rasmus_nyman@hotmail.com>
2020-12-01 06:58:43 +0100

    2020 Started! Day 1 done

---
+++ 2020/src/advent.erl
@@ -0,0 +1,181 @@
+-module(advent).
+-export([run/0, run/1, run/2, run/3, average/0,average/1,average/2,average/3,average/4]).
+
+file(Name) when is_atom(Name) ->
+    file([atom_to_list(Name),".data"]);
+
+file(["../"|_] = Name) ->
+    Name;
+file(["./"|_] = Name) ->
+    Name;
+file(["/"|_] = Name) ->
+    Name;
+
+file(Name) ->
+    Src = filename:dirname(?FILE),
+    filename:join([Src, "../data/", Name]).
+
+days() ->
+    To = case calendar:universal_time()  of
+	     {Date, _} when Date > {2020,12,25} ->
+		 25;
+	     {{2020,12, Day}, Time} when Time >= {5,0,0} ->
+		 Day;
+	     {{2020,12, Day}, _} ->
+		 Day -1
+	 end,
+    days(To).
+
+days(To) ->
+    [begin
+	 String = lists:flatten(["day", integer_to_list(Day)]),
+	 list_to_atom(String)
+     end || Day <- lists:seq(1,To)].
+
+run() ->
+    Results = [execute(Day) || Day <- days()],
+    print(Results).
+
+run(today) ->
+    run(lists:last(days()));
+
+run(Day) ->
+   run(Day, all).
+
+run(today, Star) ->
+    run(lists:last(days()), Star);
+run(Day, Star) when is_atom(Star) ->
+    run(Day, Star, file(Day));
+
+run(Day, File) ->
+    run(Day, all, file(File)).
+
+
+run(Day, all, File) ->
+    print([sep |[execute(Day, Star, file(File)) || Star <- [star1, star2]]]);   
+
+run(Day, Star, File) ->
+    print([sep, execute(Day, Star, file(File))]).
+
+
+
+average() ->
+    average(10).
+
+average(Day) when is_atom(Day) ->
+    average(10, Day);
+
+average(Times) ->
+    Results = [execute_average(Times,Day) || Day <- days()],
+    print(Results).
+
+
+average(Times,today) ->
+    average(Times,lists:last(days()));
+
+average(Times,Day) ->
+   average(Times,Day, all).
+
+average(Times,today, Star) ->
+    average(Times,lists:last(days()), Star);
+average(Times,Day, Star) when is_atom(Star) ->
+    average(Times,Day, Star, file(Day));
+
+average(Times,Day, File) ->
+    average(Times,Day, all, file(File)).
+
+
+average(Times,Day, all, File) ->
+    print([sep |[execute_average(Times,Day, Star, file(File)) || Star <- [star1, star2]]]);   
+
+average(Times,Day, Star, File) ->
+    print([sep, execute_average(Times,Day, Star, file(File))]).
+
+
+execute_average(Times, Day) ->
+    Res = [execute_average(Times, Day, Star, file(Day)) || Star <- [star1, star2]],
+    [sep | Res].
+
+execute_average(Times, Day, Star, File) when Times > 1 ->
+    Runs = [ execute(Day, Star, File) || _ <- lists:seq(1,Times)],
+
+    {Day, Star, _Time, Result} = hd(Runs),
+    RunTimes = [Time || {_Day, _Star, Time, _Result} <- Runs], 
+    Total = lists:sum(RunTimes),
+    {Day, Star, Total div Times, Result}.
+    
+
+
+parallel_average(Times, Day) ->
+    Res = [parallel_average(Times, Day, Star, file(Day)) || Star <- [star1, star2]],
+    [sep | Res].
+
+parallel_average(Times, Day, Star, File)->
+    Master = self(),
+    Run = fun() -> Master ! {self(), Master, execute(Day, Star, File)} end,
+    
+    Pids = [spawn_link(Run) || _ <- lists:seq(1,Times)],
+
+    Runs = [receive
+		 {Pid, Master, Result} ->
+		    Result
+	    end
+	    || Pid <- Pids],
+    
+    {Day, Star, _Time, Result} = hd(Runs),
+    RunTimes = [Time || {_Day, _Star, Time, _Result} <- Runs], 
+    Total = lists:sum(RunTimes),
+    {Day, Star, Total div Times, Result}.
+
+execute(Day) ->
+    Res = [execute(Day, Star, file(Day)) || Star <- [star1, star2]],
+    [sep | Res].
+
+execute(Day, Star, File) ->
+    flush(),
+    io:format("~s:run(~p, ~p).~n", [Day, Star,File]),
+    try timer:tc(Day, run, [Star, File]) of
+	{Time, Res} -> 
+	    io:format("~s:run(~p, ~p) ->~n    ~p.~n", [Day, Star,File, Res]),
+	    case Res of 
+		{Result, _Info} ->
+		    {Day, Star, Time, Result};
+		Result ->
+		    {Day, Star, Time, Result}
+	    end
+		
+    catch
+    	error:undef:Stack -> 
+	    case hd(Stack) of
+		{Day, run, [Star, File], _ } ->
+		    {Day, Star, 0, {error, undef}};
+		_ ->
+		    error({undef, Stack})
+	    end
+    end.
+
+print(head)->
+    io:format("Day   | Star  |      Time    | Result~n");
+print(sep)->
+    io:format("------+-------+--------------+----------------~n");
+
+print({Day, Star, Time, Answer} = Result) ->
+    io:format("~-5s | ~-5s | ~9.3f ms | ~p~n", [Day, Star, Time/1000, Answer]),
+    Result;
+
+print(List) ->
+    io:nl(),
+    print(head),
+    Results = lists:flatten(List),
+    [print(Result) || Result <- Results],
+    print(sep),
+    lists:filter(fun (E) -> E /= sep end, Results).
+
+
+
+flush() ->
+    receive
+	_ -> flush()
+    after
+	0 -> ok
+    end.
---
+++ 2020/src/day1.erl
@@ -0,0 +1,41 @@
+-module(day1).
+-export([run/2]).
+
+run(Star, File) ->
+    {ok, Device} = file:open(File, [read]),
+    Data = read_data(Device),
+    case Star of
+	star1 ->
+	    star1(Data);
+	star2 ->
+	    star2(Data);
+	_ ->
+	    Star1 = star1(Data),
+	    Star2 = star2(Data),
+	    {Star1, Star2}
+    end.
+
+star1(Data) ->
+    [{Value1+Value2, Value1, Value2, Value1*Value2} 
+     || Value1 <- Data, Value2 <- Data, Value1+Value2 == 2020, Value1 =< Value2].   
+
+star2(Data) ->
+    [{Value1+Value2+Value3, Value1, Value2, Value3, Value1*Value2*Value3} 
+     || Value1 <- Data, Value2 <- Data, Value3 <- Data,
+	Value1+Value2 +Value3 == 2020, Value1 =< Value2, Value2 =< Value3].
+
+read_data(Device) ->
+    read_data(Device, []).
+
+read_data(Device, Acc) ->
+    case io:fread(Device, [], "~d") of
+	eof ->
+	    lists:reverse(Acc);
+	{ok, [D]} ->
+	    read_data(Device, [D | Acc]);
+	{error, What} ->
+	    io:format("io:fread error: ~w~n", [What]),
+	    read_data(Device, Acc)
+    end.
+
+  
