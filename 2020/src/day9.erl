9fab11477af4c462cfe8aa7fd16a2dfe1343e5a3
swerasnym <rasmus_nyman@hotmail.com>
2020-12-09 07:15:43 +0100

    Slight refactor of day9

--- 2020/src/day9.erl
+++ 2020/src/day9.erl
@@ -18,38 +18,34 @@ run(Star, File) ->
 
 star1(Data) ->
     {Preamble, Values} = lists:split(25, Data),
-    check(Preamble, Values).
+    find_error(Preamble, Values).
 
 star2(Data) ->
-    {Preamble, Values} = lists:split(25, Data),
-    Result = check(Preamble, Values),
-    Sequence = find_contiious(Result, Data),
+    Result = star1(Data),
+    Sequence = find_sequence(Result, Data),
     lists:min(Sequence) + lists:max(Sequence).
 
 read(File) ->
     {ok, Bin} = file:read_file(File),
-    [transform(Line)
+    [list_to_integer(Line)
      || Line
             <- string:split(
                    string:trim(binary_to_list(Bin)), "\n", all)].
 
-transform(Line) ->
-    list_to_integer(Line).
-
-check(Preamble = [_First | Rest], [Value | Values]) ->
-    case length([A + B || A <- Preamble, B <- Preamble, A + B == Value]) of
+find_error(Preamble = [_First | Rest], [Value | Values]) ->
+    case length([A + B || A <- Preamble, B <- Preamble, A < B, A + B == Value]) of
         0 ->
             Value;
         _ ->
-            check(Rest ++ [Value], Values)
+            find_error(Rest ++ [Value], Values)
     end.
 
-find_contiious(Result, Data) ->
-    find_contiious(Result, Data, [], 0).
+find_sequence(Result, Data) ->
+    find_sequence(Result, Data, [], 0).
 
-find_contiious(Result, Data, Sequence, Sum) when Sum == Result ->
+find_sequence(Result, _Data, Sequence, Sum) when Sum == Result ->
     Sequence;
-find_contiious(Result, Data, [First | Rest], Sum) when Sum > Result ->
-    find_contiious(Result, Data, Rest, Sum - First);
-find_contiious(Result, [First | Rest], Sequence, Sum) ->
-    find_contiious(Result, Rest, Sequence ++ [First], Sum + First).
+find_sequence(Result, Data, [First | NewSequence], Sum) when Sum > Result ->
+    find_sequence(Result, Data, NewSequence, Sum - First);
+find_sequence(Result, [First | NewData], Sequence, Sum) ->
+    find_sequence(Result, NewData, Sequence ++ [First], Sum + First).
