70cccd60ec21c786cac0fe1b77a45d2fdab0d709
swerasnym <rasmus_nyman@hotmail.com>
2020-12-15 07:40:12 +0100

    Format days 12..15

--- 2020/src/day14.erl
+++ 2020/src/day14.erl
@@ -17,72 +17,76 @@ run(Star, File) ->
     end.
 
 star1(Data) ->
-    {_Mask, Memory} = lists:foldl(fun process/2, {{mask, 0,0} ,#{}}, Data),
-    lists:sum(maps:values(Memory)).
+    {_Mask, Memory} = lists:foldl(fun process/2, {{mask, 0, 0}, #{}}, Data),
+    lists:sum(
+        maps:values(Memory)).
 
 star2(Data) ->
-   {_Mask, Memory} = lists:foldl(fun process2/2, {{mask, 0,0} ,#{}}, Data),
-    lists:sum(maps:values(Memory)).
-
+    {_Mask, Memory} = lists:foldl(fun process2/2, {{mask, 0, 0}, #{}}, Data),
+    lists:sum(
+        maps:values(Memory)).
 
 read(File) ->
     {ok, Bin} = file:read_file(File),
-    [ split(Line)
-      %% list_to_integer(Line)
+    [split(Line)
+     %% list_to_integer(Line)
      || Line
             <- string:split(
                    string:trim(binary_to_list(Bin)), "\n", all)].
 
-
-split("mask = "++Mask) ->
-    And = list_to_integer(lists:flatten(string:replace(Mask,"X",$1,all)),2),
-    Or = list_to_integer(lists:flatten(string:replace(Mask,"X",$0,all)),2),
+split("mask = " ++ Mask) ->
+    And = list_to_integer(lists:flatten(
+                              string:replace(Mask, "X", $1, all)),
+                          2),
+    Or = list_to_integer(lists:flatten(
+                             string:replace(Mask, "X", $0, all)),
+                         2),
 
     {mask, And, Or, Mask};
-split("mem["++Rest) ->
-    {Pos, "] = "++ValueS} = string:to_integer(Rest),
+split("mem[" ++ Rest) ->
+    {Pos, "] = " ++ ValueS} = string:to_integer(Rest),
     {Value, []} = string:to_integer(ValueS),
     {mem, Pos, Value}.
-
-    
 
 process({mask, _And, _Or, _} = Mask, {_, Mem}) ->
     {Mask, Mem};
 process({mem, Pos, Value}, {{mask, And, Or} = Mask, Mem}) ->
-    {Mask,  Mem#{Pos=> (Value band And) bor Or}}.
+    {Mask, Mem#{Pos => Value band And bor Or}}.
 
-process2({mask, _And, _Or,M} = Mask, {_, Mem}) ->
+process2({mask, _And, _Or, M} = Mask, {_, Mem}) ->
     {{Mask, generate_masks(M)}, Mem};
+process2({mem, Pos, Value}, {{{mask, _And, Or, M}, OrList} = Mask, Mem}) ->
+    AndMask =
+        lists:flatten(
+            string:replace(M, "0", $1, all)),
+    BaseMask =
+        list_to_integer(lists:flatten(
+                            string:replace(AndMask, "X", $0, all)),
+                        2),
 
-process2({mem, Pos, Value}, {{{mask, _And, Or, M}, OrList }= Mask, Mem}) ->
-  
+    BasePos = Pos band BaseMask bor Or,
 
-    AndMask = lists:flatten(string:replace(M ,"0",$1, all)),
-    BaseMask = list_to_integer(lists:flatten(string:replace(AndMask,"X",$0,all)),2),
+    NewMap = lists:foldl(fun(OrI, Map) -> Map#{BasePos bor OrI => Value} end, Mem, OrList),
+    {Mask, NewMap}.
 
-    BasePos = (Pos band BaseMask) bor Or,
+generate_masks(Mask) ->
+    OrMask =
+        lists:flatten(
+            string:replace(Mask, "1", $0, all)),
+    generate_masks([OrMask], []).
 
-    NewMap = lists:foldl(fun ( OrI , Map) -> 
-			
-        
-				 Map#{(BasePos bor OrI) => Value} 
-			 end, Mem, OrList),
-    {Mask,  NewMap}.
-
-generate_masks(Mask) -> 
-    OrMask = lists:flatten(string:replace(Mask ,"1",$0, all)),
-    generate_masks([OrMask],[]).
-    
 generate_masks([], Acc) ->
     Acc;
-generate_masks([Mask|Masks], Acc) ->
-    case lists:flatten(string:replace(Mask,"X",$1)) of
-	Mask ->
-	    generate_masks(Masks, [list_to_integer(Mask,2) |Acc]);
-	Mask2 ->
-	    generate_masks([Mask2,  lists:flatten(string:replace(Mask,"X",$0)) | Masks], Acc)
+generate_masks([Mask | Masks], Acc) ->
+    case lists:flatten(
+             string:replace(Mask, "X", $1))
+    of
+        Mask ->
+            generate_masks(Masks, [list_to_integer(Mask, 2) | Acc]);
+        Mask2 ->
+            generate_masks([Mask2,
+                            lists:flatten(
+                                string:replace(Mask, "X", $0))
+                            | Masks],
+                           Acc)
     end.
-
-   
-
-    
--- 2020/src/day15.erl
+++ 2020/src/day15.erl
@@ -3,9 +3,8 @@
 -export([run/2]).
 
 run(Star, File) ->
-    Data = [8,11,0,19,1,2],
-   %  Data = [0,3,6],
-
+    Data = [8, 11, 0, 19, 1, 2],
+    %  Data = [0,3,6],
     case Star of
         star1 ->
             star1(Data);
@@ -18,54 +17,51 @@ run(Star, File) ->
     end.
 
 star1(Data) ->
-    TurnMap = maps:from_list(lists:zip(Data, lists:seq(1,length(Data)))),
-    speek(2020, 0, length(Data) +1 , TurnMap).
-
-    
+    TurnMap =
+        maps:from_list(
+            lists:zip(Data, lists:seq(1, length(Data)))),
+    speek(2020, 0, length(Data) + 1, TurnMap).
 
 star2(Data) ->
     ets:new(star2, [set, named_table]),
-    [ets:insert(star2, Pair) || Pair <- lists:zip(Data, lists:seq(1,length(Data)))],
-    speek_ets(30000000, 0, length(Data) +1 , star2).
-    
-    
+    [ets:insert(star2, Pair) || Pair <- lists:zip(Data, lists:seq(1, length(Data)))],
+    speek_ets(30000000, 0, length(Data) + 1, star2).
 
 speek(Goal, Last, Goal, TurnMap) ->
     Last;
-speek(Goal, Last, Turn, TurnMap)->
-    Speek = case maps:get(Last, TurnMap, 0) of
-		0 ->
-		    0;
-		N ->
-		    Turn - N
-
-	    end,
+speek(Goal, Last, Turn, TurnMap) ->
+    Speek =
+        case maps:get(Last, TurnMap, 0) of
+            0 ->
+                0;
+            N ->
+                Turn - N
+        end,
     case Turn rem 1000000 of
-	0 ->
-	    io:format("Turn ~p: ~p -> ~p ~n",[Turn, Last,  Speek]);
-	_ ->
-	    ok
+        0 ->
+            io:format("Turn ~p: ~p -> ~p ~n", [Turn, Last, Speek]);
+        _ ->
+            ok
     end,
-    
-    speek(Goal, Speek, Turn+1, TurnMap#{Last => Turn}).
+
+    speek(Goal, Speek, Turn + 1, TurnMap#{Last => Turn}).
 
 speek_ets(Goal, Last, Goal, Table) ->
     ets:delete(Table),
     Last;
-
-speek_ets(Goal, Last, Turn, Table)->
-    Speek = case ets:lookup(Table, Last) of
-		[] ->
-		    0;
-		[{Last, N}] ->
-		    Turn - N
-
-	    end,
+speek_ets(Goal, Last, Turn, Table) ->
+    Speek =
+        case ets:lookup(Table, Last) of
+            [] ->
+                0;
+            [{Last, N}] ->
+                Turn - N
+        end,
     case Turn rem 1000000 of
-	0 ->
-	    io:format("Turn ~p: ~p -> ~p ~n",[Turn, Last,  Speek]);
-	_ ->
-	    ok
+        0 ->
+            io:format("Turn ~p: ~p -> ~p ~n", [Turn, Last, Speek]);
+        _ ->
+            ok
     end,
     ets:insert(Table, {Last, Turn}),
-    speek_ets(Goal, Speek, Turn+1, Table).
+    speek_ets(Goal, Speek, Turn + 1, Table).
