d11a7d7135210349b308aa6480fc9d69d6e67dfe
swerasnym <rasmus_nyman@hotmail.com>
2021-12-22 12:49:56 +0100

    2021 day 22 readability with records

--- 2021/src/aoc2021_day22.erl
+++ 2021/src/aoc2021_day22.erl
@@ -1,12 +1,16 @@
 -module(aoc2021_day22).
 
--export([run/2, profile/3, eprof/2, split/2]).
+-export([run/2, profile/3, eprof/2]).
+
+-record(cube, {state, x, y, z}).
 
 run(Star, File) ->
     Data = read(File),
     case Star of
         star1 ->
             star1(Data);
+        star1old ->
+            star1old(Data);
         star2 ->
             star2(Data);
         _ ->
@@ -47,12 +51,19 @@ eprof(Star, File) ->
     eprof:stop(),
     Result.
 
-star1(Data) ->
+star1old(Data) ->
     Init = lists:filter(fun is_init/1, Data),
     Steps = lists:map(fun discretre/1, Init),
     Reactor =
         lists:foldl(fun({K, V}, Map) -> maps:put(K, V, Map) end, #{}, lists:flatten(Steps)),
     tools:count(on, Reactor).
+
+star1(Data) ->
+    Init = lists:filter(fun is_init/1, Data),
+    Pices = remove_overlaps(Init),
+    On = on(Pices),
+    lists:sum(
+        lists:map(fun volume/1, On)).
 
 star2(Data) ->
     Pices = remove_overlaps(Data),
@@ -61,100 +72,76 @@ star2(Data) ->
         lists:map(fun volume/1, On)).
 
 read(File) ->
-    [{S, {X_min, X_Max + 1}, {Y_min, Y_Max + 1}, {Z_min, Z_Max + 1}}
+    [#cube{state = S,
+           x = {X_min, X_Max + 1},
+           y = {Y_min, Y_Max + 1},
+           z = {Z_min, Z_Max + 1}}
      || [S, X_min, X_Max, Y_min, Y_Max, Z_min, Z_Max]
             <- tools:read_format(File, "~a x=~d..~d,y=~d..~d,z=~d..~d")].
 
-is_init({_, {X_min, X_Max}, {Y_min, Y_Max}, {Z_min, Z_Max}})
-    when X_min >= -50, X_Max =< 50, Y_min >= -50, Y_Max =< 50, Z_min >= -50, Z_Max =< 50 ->
+is_init(#cube{x = {Xmin, XMax},
+              y = {Ymin, YMax},
+              z = {Zmin, ZMax}})
+    when Xmin >= -50, XMax =< 50, Ymin >= -50, YMax =< 50, Zmin >= -50, ZMax =< 50 ->
     true;
-is_init(_) ->
+is_init(#cube{}) ->
     false.
 
-discretre({S, {X_min, X_Max}, {Y_min, Y_Max}, {Z_min, Z_Max}}) ->
+discretre(#cube{state = S,
+                x = {Xmin, Xmax},
+                y = {Ymin, Ymax},
+                z = {Zmin, Zmax}}) ->
     [{{X, Y, Z}, S}
-     || X <- lists:seq(X_min, X_Max - 1),
-        Y <- lists:seq(Y_min, Y_Max - 1),
-        Z <- lists:seq(Z_min, Z_Max - 1)].
+     || X <- lists:seq(Xmin, Xmax - 1),
+        Y <- lists:seq(Ymin, Ymax - 1),
+        Z <- lists:seq(Zmin, Zmax - 1)].
 
 on(List) ->
-    lists:filter(fun({S, _, _, _}) -> S == on end, List).
+    lists:filter(fun(#cube{state = S}) -> S == on end, List).
 
-overlaps({_S1, {_X_min1, X_Max1}, _Y1, _Z1}, {_S2, {X_min2, _X_Max2}, _Y2, _Z2})
-    when X_min2 >= X_Max1 ->
+overlaps(#cube{x = {Min1, Max1}}, #cube{x = {Min2, Max2}})
+    when Min2 >= Max1; Min1 >= Max2 ->
     false;
-overlaps({_S1, {X_min1, _X_Max1}, _Y1, _Z1}, {_S2, {_X_min2, X_Max2}, _Y2, _Z2})
-    when X_min1 >= X_Max2 ->
+overlaps(#cube{y = {Min1, Max1}}, #cube{y = {Min2, Max2}})
+    when Min2 >= Max1; Min1 >= Max2 ->
     false;
-overlaps({_S1, _X1, {_Y_min1, Y_Max1}, _Z1}, {_S2, _X2, {Y_min2, _Y_Max2}, _Z2})
-    when Y_min2 >= Y_Max1 ->
-    false;
-overlaps({_S1, _X1, {Y_min1, _Y_Max1}, _Z1}, {_S2, _X2, {_Y_min2, Y_Max2}, _Z2})
-    when Y_min1 >= Y_Max2 ->
-    false;
-overlaps({_S1, _X1, _Y1, {_Z_min1, Z_Max1}}, {_S2, _X2, _Y2, {Z_min2, _Z_Max2}})
-    when Z_min2 >= Z_Max1 ->
-    false;
-overlaps({_S1, _X1, _Y1, {Z_min1, _Z_Max1}}, {_S2, _X2, _Y2, {_Z_min2, Z_Max2}})
-    when Z_min1 >= Z_Max2 ->
+overlaps(#cube{z = {Min1, Max1}}, #cube{z = {Min2, Max2}})
+    when Min2 >= Max1; Min1 >= Max2 ->
     false;
 overlaps(_, _) ->
     true.
 
-split_x(0, {_, X, _, _} = C1, {_, X, _, _} = C2) ->
-    {[C1, C2], []};
-split_x(N, {S1, {X1_min, X1_Max}, Y1, Z1}, {S2, {X2_min, X2_Max}, Y2, Z2})
-    when N < 2, X1_min =< X2_min, X2_min < X1_Max, X1_Max =< X2_Max ->
-    % {[Overlapp], [NonOverlap]}
-    {[{S1, {X2_min, X1_Max}, Y1, Z1}, {S2, {X2_min, X1_Max}, Y2, Z2}],
-     [{S1, {X1_min, X2_min}, Y1, Z1}, {S2, {X1_Max, X2_Max}, Y2, Z2}]};
-split_x(N, {S1, {X1_min, X1_Max}, Y1, Z1}, {S2, {X2_min, X2_Max}, Y2, Z2})
-    when N < 2, X1_min =< X2_min, X1_Max >= X2_Max ->
-    {[{S1, {X2_min, X2_Max}, Y1, Z1}, {S2, {X2_min, X2_Max}, Y2, Z2}],
-     [{S1, {X1_min, X2_min}, Y1, Z1}, {S1, {X2_Max, X1_Max}, Y1, Z1}]};
-split_x(N, C1, C2) when N < 2 ->
-    {[O2, O1], No} = split_x(N + 1, C2, C1),
-    {[O1, O2], No}.
-
-split_y(0, {_, _, Y, _} = C1, {_, _, Y, _} = C2) ->
-    {[C1, C2], []};
-split_y(N, {S1, X1, {Y1_min, Y1_Max}, Z1}, {S2, X2, {Y2_min, Y2_Max}, Z2})
-    when N < 2, Y1_min =< Y2_min, Y2_min < Y1_Max, Y1_Max =< Y2_Max ->
-    % {[Overlapp], [NonOverlap]}
-    {[{S1, X1, {Y2_min, Y1_Max}, Z1}, {S2, X2, {Y2_min, Y1_Max}, Z2}],
-     [{S1, X1, {Y1_min, Y2_min}, Z1}, {S2, X2, {Y1_Max, Y2_Max}, Z2}]};
-split_y(N, {S1, X1, {Y1_min, Y1_Max}, Z1}, {S2, X2, {Y2_min, Y2_Max}, Z2})
-    when N < 2, Y1_min =< Y2_min, Y1_Max >= Y2_Max ->
-    {[{S1, X1, {Y2_min, Y2_Max}, Z1}, {S2, X2, {Y2_min, Y2_Max}, Z2}],
-     [{S1, X1, {Y1_min, Y2_min}, Z1}, {S1, X1, {Y2_Max, Y1_Max}, Z1}]};
-split_y(N, C1, C2) when N < 2 ->
-    {[O2, O1], No} = split_y(N + 1, C2, C1),
-    {[O1, O2], No}.
-
-split_z(0, {_, _, _, Z} = C1, {_, _, _, Z} = C2) ->
-    {[C1, C2], []};
-split_z(N, {S1, X1, Y1, {Z1_min, Z1_Max}}, {S2, X2, Y2, {Z2_min, Z2_Max}})
-    when N < 2, Z1_min =< Z2_min, Z2_min < Z1_Max, Z1_Max =< Z2_Max ->
-    % {[Overlapp], [NonOverlap]}
-    {[{S1, X1, Y1, {Z2_min, Z1_Max}}, {S2, X2, Y2, {Z2_min, Z1_Max}}],
-     [{S1, X1, Y1, {Z1_min, Z2_min}}, {S2, X2, Y2, {Z1_Max, Z2_Max}}]};
-split_z(N, {S1, X1, Y1, {Z1_min, Z1_Max}}, {S2, X2, Y2, {Z2_min, Z2_Max}})
-    when N < 2, Z1_min =< Z2_min, Z1_Max >= Z2_Max ->
-    {[{S1, X1, Y1, {Z2_min, Z2_Max}}, {S2, X2, Y2, {Z2_min, Z2_Max}}],
-     [{S1, X1, Y1, {Z1_min, Z2_min}}, {S1, X1, Y1, {Z2_Max, Z1_Max}}]};
-split_z(N, C1, C2) when N < 2 ->
-    {[O2, O1], No} = split_z(N + 1, C2, C1),
-    {[O1, O2], No}.
+%% Rettunrs {[Overlapp], [NonOverlap]} parts of the second cube is always last.
+split(Dir, C1, C2) ->
+    L1 = {Min1, Max1} = element(Dir, C1),
+    L2 = {Min2, Max2} = element(Dir, C2),
+    if L1 == L2 ->
+           {[C1, C2], []};
+       Min1 =< Min2, Min2 < Max1, Max1 =< Max2 ->
+           {[setelement(Dir, C1, {Min2, Max1}), setelement(Dir, C2, {Min2, Max1})],
+            [setelement(Dir, C1, {Min1, Min2}), setelement(Dir, C2, {Max1, Max2})]};
+       Min1 =< Min2, Max1 >= Max2 ->
+           {[setelement(Dir, C1, L2), C2],
+            [setelement(Dir, C1, {Min1, Min2}), setelement(Dir, C1, {Max2, Max1})]};
+       Min2 =< Min1, Min1 < Max2, Max2 =< Max1 ->
+           {[setelement(Dir, C1, {Min1, Max2}), setelement(Dir, C2, {Min1, Max2})],
+            [setelement(Dir, C1, {Max2, Max1}), setelement(Dir, C2, {Min2, Min1})]};
+       Min2 =< Min1, Max2 >= Max1 ->
+           {[C1, setelement(Dir, C2, L1)],
+            [setelement(Dir, C2, {Min2, Min1}), setelement(Dir, C2, {Max1, Max2})]}
+    end.
 
 split(C1, C2) ->
-    {[X1, X2], Xn} = split_x(0, C1, C2),
-    {[Y1, Y2], Yn} = split_y(0, X1, X2),
-    {[_Z1, Z2], Zn} = split_z(0, Y1, Y2),
+    {[X1, X2], Xn} = split(#cube.x, C1, C2),
+    {[Y1, Y2], Yn} = split(#cube.y, X1, X2),
+    {[_Z1, Z2], Zn} = split(#cube.z, Y1, Y2),
 
     % Z1 discarded since that contains the overlappng part in C1 that we are replaceing.
     lists:filter(fun(V) -> volume(V) /= 0 end, Xn ++ Yn ++ Zn ++ [Z2]).
 
-volume({_, {Xmin, Xmax}, {Ymin, Ymax}, {Zmin, Zmax}}) ->
+volume(#cube{x = {Xmin, Xmax},
+             y = {Ymin, Ymax},
+             z = {Zmin, Zmax}}) ->
     (Xmax - Xmin) * (Ymax - Ymin) * (Zmax - Zmin).
 
 remove_overlaps([C1 | Rest]) ->
