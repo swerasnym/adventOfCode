8a93a66054ecd464c09a306fd06befb05784e17e
swerasnym <rasmus_nyman@hotmail.com>
2021-12-23 11:35:59 +0100

    2021 Day 23 done

--- 2021/src/aoc2021_day23.erl
+++ 2021/src/aoc2021_day23.erl
@@ -43,34 +43,15 @@ eprof(Star, File) ->
     eprof:start_profiling([self()]),
     Result = run(Star, File),
     eprof:stop_profiling(),
-    eprof:analyze(),
+    eprof:analyze(total),
     eprof:stop(),
     Result.
 
-star1({Start, Goal}) ->
-   %%  maps:from_list(ends()).
-    
+star1({{Start, Goal}, _}) ->
+    bfs([{0, Start}], Goal).
 
-    %% [{Pos, maps:get(Pos, Goal)} || Pos <- valid()].
-
-    bfs([{0,{0,Start}}], Goal).
-
-star2({Start, Goal}) ->
-    dfs($D, Start, Goal).
-
-    %% tools:print_grid(Start),
-    %% io:format("~n~n"),
-    %% tools:print_grid(Goal),
-    
-    %% tools:print_grid(map_of($A, Start)),
-    %% io:format("~n~n"),
-    %% tools:print_grid(map_of($B, Start)),
-    %% io:format("~n~n"),
-
-    %% tools:print_grid(map_of($C, Start)),
-    %% io:format("~n~n"),
-    %% tools:print_grid(map_of($D, Start)),
-    %% io:format("~n~n").
+star2({_, {Start, Goal}}) ->
+    bfs([{0, Start}], Goal).
 
 read(File) ->
     erase(),
@@ -80,65 +61,112 @@ read(File) ->
 ###A#B#C#D###
   #A#B#C#D#
   #########",
-    {Valid, _} = lists:unzip(neigbours()), 
 
-    {maps:with(Valid, tools:read_grid(File)), maps:with( Valid, tools:parse_grid(Goal))}.
+    Goal2 =
+        "#############
+#...........#
+###A#B#C#D###
+  #A#B#C#D#
+  #A#B#C#D#
+  #A#B#C#D#
+  #########",
+    L4 = "  #D#C#B#A#",
+    L5 = "  #D#B#A#C#",
 
-bfs([ {_Estimate, {Cost, Goal}}], Goal) ->
-    tools:print_grid(Goal),
-    io:format("~n~p~n", [Cost]),
+    [L1, L2, L3, L6, L7] = tools:read_lines(File),
+
+    Start2 = string:join([L1, L2, L3, L4, L5, L6, L7], "\n"),
+
+    {{tools:read_grid(File), tools:parse_grid(Goal)},
+     {tools:parse_grid(Start2), tools:parse_grid(Goal2)}}.
+
+starts(Map) ->
+    maps:to_list(
+        maps:filter(fun amphipod/2, Map)).
+
+bfs([{Cost, Goal} | _Rest], Goal) ->
     Cost;
-bfs([{_Estimate, {Cost, State}} | Rest], Goal) ->
-    %% case get(State) of
-    %% 	undefined  ->
-    %% 	    put(State, visited),
-    %% 	   %%  tools:print_grid(State),
-    %% 	   %%  io:format("~n~p~n", [Cost]),
-    %% 	    bfs(lists:umerge(Rest, next(Hd)), Goal);
-    %% 	visited ->
-    %% 	    bfs(Rest, Goal)
-    %% end.
-    bfs(lists:umerge(Rest, next({Cost, State}, Goal)), Goal).	
+bfs([{Cost, Current} | Rest], Goal) ->
+    case get(Current) of
+        undefined ->
+            put(Current, visited),
+            Moves = moves(Current, Goal),
+            Next =
+                [{Cost + cost(Start, End, Type), Current#{Start => $., End => Type}}
+                 || {Start, End, Type} <- Moves],
+            bfs(lists:umerge(Rest, lists:sort(Next)), Goal);
+        visited ->
+            bfs(Rest, Goal)
+    end.
 
-    %% %% tools:print_grid(First),
-    %% %% io:format("~n~p~n", [Cost]),
+moves(Map, Goal) ->
+    [{Start, End, Type}
+     || {Start, Type} <- starts(Map),
+        End <- single_moves(Start, Map),
+        valid_space(End),
+        type(Start) /= type(End),
+        leave_room(Start, Type, Map, Goal),
+        enter_room(End, Type, Map, Goal)].
 
-%% bfs(lists:umerge(Rest, next(Hd)), Goal).
+single_moves(Pos, Map) ->
+    Moves = [N || N <- neigbours(Pos), maps:get(N, Map, $#) == $.],
+    Filled = maps:merge(Map, maps:from_keys(Moves, $!)),
 
-next({Cost, State}, Goal) ->
-    Moves = [{Start, End, Type} || {Start, Neigbours} <- neigbours(),
-				   (Type = maps:get(Start, State)) /= $., may_move(Start, Type, State),
+    PossibleMoves = lists:flatten(Moves ++ [single_moves(N, Filled) || N <- Moves]),
+    [M || M <- PossibleMoves].
 
-				   End <-  valid_moves(Neigbours, Type, State)],
+neigbours({X, Y}) ->
+    [{X, Y - 1}, {X, Y + 1}, {X - 1, Y}, {X + 1, Y}].
 
-    Next = [{Cost + cost(Start, End, Type), 
-			 State#{Start => $., End => Type}}
-			|| {Start, End, Type} <- Moves],
+amphipod(_, $A) ->
+    true;
+amphipod(_, $B) ->
+    true;
+amphipod(_, $C) ->
+    true;
+amphipod(_, $D) ->
+    true;
+amphipod(_, _) ->
+    false.
 
+type({_, 1}) ->
+    hall;
+type({_, _}) ->
+    room.
 
-    WCost = [begin
-	 put(S, visited),
-	 
-	 
-	 {C + dfs($A, S, Goal) + dfs($B, S, Goal) +dfs($C, S, Goal) + dfs($D, S, Goal)  , V}
-	     end || V = {C ,S} <- Next, get(S) == undefined],
-    
-    lists:usort(WCost).
+enter_room({_X, 1}, _Type, _Map, _Goal) ->
+    true;
+enter_room(Pos = {X, Y}, Type, Map, Goal) ->
+    maps:get(Pos, Goal) == Type andalso check_room(Type, {X, Y + 1}, Map).
 
+leave_room({_X, 1}, _Type, _Map, _Goal) ->
+    true;
+leave_room(Pos = {X, Y}, Type, Map, Goal) ->
+    maps:get(Pos, Goal) /= Type orelse not check_room(Type, {X, Y + 1}, Map).
 
+check_room(Type, {X, Y}, Map) ->
+    case maps:get({X, Y}, Map) of
+        $# ->
+            true;
+        Type ->
+            check_room(Type, {X, Y + 1}, Map);
+        _ ->
+            false
+    end.
 
+valid_space({3, 1}) ->
+    false;
+valid_space({5, 1}) ->
+    false;
+valid_space({7, 1}) ->
+    false;
+valid_space({9, 1}) ->
+    false;
+valid_space({_, _}) ->
+    true.
 
-
-    %% %    maps:find
-    %% [{Cost, Goal}].
-
-cost({X1, Y1}, {X2, Y2}, T) ->
-    (abs(X1 - X2) + abs(Y1 - Y2)) * c(T).
-
-
-cost({X1, Y1}, {X2, Y2}) ->
-    (abs(X1 - X2) + abs(Y1 - Y2)).
-    
+cost({X1, Y1}, {X2, Y2}, Type) ->
+    (abs(X1 - X2) + abs(Y1 - Y2)) * c(Type).
 
 c($A) ->
     1;
@@ -148,135 +176,3 @@ c($C) ->
     100;
 c($D) ->
     1000.
-
-
-
-
-
-%% #############
-%% #12.4.6.8.01#
-%% ###B#C#B#D###
-%%   #A#D#C#A#
-%%   #########
-
-neigbours() ->
-    [{{1, 1}, [{2, 1}]},
-     {{2, 1}, [{1, 1}, {4, 1}, {3, 2}]},
-     {{4, 1}, [{2, 1}, {6, 1}, {3, 2}, {5, 2}]},
-     {{6, 1}, [{4, 1}, {8, 1}, {5, 2}, {7, 2}]},
-     {{8, 1}, [{6, 1}, {10, 1}, {7, 2}, {9, 2}]},
-     {{10, 1}, [{8, 1}, {11, 1}, {9, 2}]},
-     {{11, 1}, [{10, 1}]},
-     {{3, 2}, [{2, 1}, {4, 1}, {3, 3}]},
-     {{3, 3}, [{3, 2}]},
-     {{5, 2}, [{4, 1}, {6, 1}, {5, 3}]},
-     {{5, 3}, [{5, 2}]},
-     {{7, 2}, [{6, 1}, {8, 1}, {7, 3}]},
-     {{7, 3}, [{7, 2}]},
-     {{9, 2}, [{8, 1}, {10, 1}, {9, 3}]},
-     {{9, 3}, [{9, 2}]}].
-
-
-neigbours_posible() ->
-    [{{1, 1}, [{2, 1}]},
-     {{2, 1}, [{1, 1}, {4, 1}, {3, 2}]},
-     {{4, 1}, [{2, 1}, {6, 1}, {3, 2}, {5, 2}]},
-     {{6, 1}, [{4, 1}, {8, 1}, {5, 2}, {7, 2}]},
-     {{8, 1}, [{6, 1}, {10, 1}, {7, 2}, {9, 2}]},
-     {{10, 1}, [{8, 1}, {11, 1}, {9, 2}]},
-     {{11, 1}, [{10, 1}]},
-     {{3, 2}, [{2, 1}, {4, 1}, {3, 3}]},
-     {{3, 3}, [{3, 2}]},
-     {{5, 2}, [{4, 1}, {6, 1}, {5, 3}]},
-     {{5, 3}, [{5, 2}]},
-     {{7, 2}, [{6, 1}, {8, 1}, {7, 3}]},
-     {{7, 3}, [{7, 2}]},
-     {{9, 2}, [{8, 1}, {10, 1}, {9, 3}]},
-     {{9, 3}, [{9, 2}]}].
-
-
-
-ends() ->
-    #{{3,2} => 65,
-      {3,3} => 65,
-      {5,2} => 66,
-      {5,3} => 66,  
-      {7,2} => 67,
-      {7,3} => 67,
-      {9,2} => 68,
-      {9,3} => 68}.
-
-may_move({_, 1}, _Type, _State) ->
-    true;
-may_move({X,3}, Type, _State) ->
-    maps:get({X,3}, ends()) /= Type;
-may_move({X,2}, Type, State) ->
-    maps:get({X,2}, ends()) /= Type orelse maps:get({X,3}, State) /= Type.
-
-
-
-    
-
-
-
-valid_moves([Hd | Rest], Type,  State) ->
-    lists:flatten(empty(Hd, State) ++ [empty(P, State) || P <- Rest, maps:get(P, ends(), Type) == Type]).
-    
-
-
-empty(Pos, State) ->
-    case maps:get(Pos, State) of
-	$. ->
-	    [Pos];
-	_ ->
-	    []
-    end.
-
-
-
-map_of(Type, State) ->
-    Map = maps:without([Type],  #{ $A => $.,
-				   $B => $.,
-				   $C => $.,
-				   $D => $.}),
-    tools:replace(State, Map , all).
-
-
-dfs(Type, Start, Goal) ->
-    c(Type)*dfs(map_of(Type, Start), map_of(Type, Goal)).
-
-dfs(Goal, Goal) ->
-    0;
-dfs(Pos, Goal) ->
-    case get(Pos) of
-
-
-	undefined ->
-	    put(Pos, visited),
-
-
-	    Next = next_dfs(Pos),
-	    Result = lists:min([dfs(N, Goal) +Dist  ||  {N, Dist} <- Next]),
-	    if Result < 1000000 ->
-		    put(Pos, Result);
-	       true ->
-		    erase(Pos)
-	    end,
-	    tools:print_grid(Pos),
-	    io:format("~n~p~n", [Result]),
-
-	    Result;
-	visited ->
-	    1000000;
-	Value ->
-	    Value
-    end.
-
-next_dfs(State) ->
-    Moves = [{State#{Start => $., End => Type},  cost(Start, End)} || {Start, Neigbours} <- neigbours(), 
-						 (Type = maps:get(Start, State)) /= $. , % , may_move(Start, Type, State),
-						 
-
-				    End <- valid_moves(Neigbours, Type, State)
-
-	    ].
