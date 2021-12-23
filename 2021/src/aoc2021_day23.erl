ed72368b35f6df58dc1cf0aac97d9677e24c82fc
swerasnym <rasmus_nyman@hotmail.com>
2021-12-23 13:40:22 +0100

    2021 Day 23 optimized

--- 2021/src/aoc2021_day23.erl
+++ 2021/src/aoc2021_day23.erl
@@ -43,19 +43,19 @@ eprof(Star, File) ->
     eprof:start_profiling([self()]),
     Result = run(Star, File),
     eprof:stop_profiling(),
-    eprof:analyze(total),
+    eprof:analyze(),
     eprof:stop(),
     Result.
 
 star1({{Start, Goal}, _}) ->
-    bfs([{0, Start}], Goal).
+    erase(),
+    bfs(gb_sets:singleton({0, remove_fluff(Start)}), remove_fluff(Goal)).
 
 star2({_, {Start, Goal}}) ->
-    bfs([{0, Start}], Goal).
+    erase(),
+    bfs(gb_sets:singleton({0, remove_fluff(Start)}), remove_fluff(Goal)).
 
 read(File) ->
-    erase(),
-
     Goal = "#############
 #...........#
 ###A#B#C#D###
@@ -80,40 +80,69 @@ read(File) ->
     {{tools:read_grid(File), tools:parse_grid(Goal)},
      {tools:parse_grid(Start2), tools:parse_grid(Goal2)}}.
 
+remove_fluff(Map) ->
+    maps:filter(fun (_, $#) ->
+                        false;
+                    (_, $ ) ->
+                        false;
+                    (_, _) ->
+                        true
+                end,
+                Map).
+
 starts(Map) ->
     maps:to_list(
         maps:filter(fun amphipod/2, Map)).
 
 bfs([{Cost, Goal} | _Rest], Goal) ->
     Cost;
-bfs([{Cost, Current} | Rest], Goal) ->
-    case get(Current) of
-        undefined ->
-            put(Current, visited),
-            Moves = moves(Current, Goal),
-            Next =
-                [{Cost + cost(Start, End, Type), Current#{Start => $., End => Type}}
-                 || {Start, End, Type} <- Moves],
-            bfs(lists:umerge(Rest, lists:sort(Next)), Goal);
-        visited ->
-            bfs(Rest, Goal)
+bfs(Set1, Goal) ->
+    {Element, Set2} = gb_sets:take_smallest(Set1),
+    case Element of
+        {Cost, Goal} ->
+            Cost;
+        {Cost, Current} ->
+            case get(Current) of
+                undefined ->
+                    put(Current, visited),
+                    Moves = moves(Current, Goal),
+                    Next =
+                        [{Cost + cost(Start, End, Type), Current#{Start => $., End => Type}}
+                         || {Start, End, Type} <- Moves],
+                    Set3 = gb_sets:union(Set2, gb_sets:from_list(Next)),
+
+                    bfs(Set3, Goal);
+                visited ->
+                    bfs(Set2, Goal)
+            end
     end.
 
 moves(Map, Goal) ->
+    Open = maps:filter(fun(_, V) -> V == $. end, Map),
+
     [{Start, End, Type}
      || {Start, Type} <- starts(Map),
-        End <- single_moves(Start, Map),
+        End <- single_moves(Start, Open),
         valid_space(End),
         type(Start) /= type(End),
         leave_room(Start, Type, Map, Goal),
         enter_room(End, Type, Map, Goal)].
 
+single_moves(_Pos, Map) when map_size(Map) == 0 ->
+    [];
 single_moves(Pos, Map) ->
-    Moves = [N || N <- neigbours(Pos), maps:get(N, Map, $#) == $.],
-    Filled = maps:merge(Map, maps:from_keys(Moves, $!)),
+    case get({moves, Pos, Map}) of
+        undefined ->
+            Moves = [N || N <- neigbours(Pos), maps:get(N, Map, $#) == $.],
+            Filled = maps:without(Moves, Map),
 
-    PossibleMoves = lists:flatten(Moves ++ [single_moves(N, Filled) || N <- Moves]),
-    [M || M <- PossibleMoves].
+            PossibleMoves = Moves ++ lists:append([single_moves(N, Filled) || N <- Moves]),
+            Result = [M || M <- PossibleMoves],
+            put({moves, Pos, Map}, Result),
+            Result;
+        Result ->
+            Result
+    end.
 
 neigbours({X, Y}) ->
     [{X, Y - 1}, {X, Y + 1}, {X - 1, Y}, {X + 1, Y}].
@@ -145,7 +174,7 @@ leave_room(Pos = {X, Y}, Type, Map, Goal) ->
     maps:get(Pos, Goal) /= Type orelse not check_room(Type, {X, Y + 1}, Map).
 
 check_room(Type, {X, Y}, Map) ->
-    case maps:get({X, Y}, Map) of
+    case maps:get({X, Y}, Map, $#) of
         $# ->
             true;
         Type ->
