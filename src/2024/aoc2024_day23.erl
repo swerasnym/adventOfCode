dadf4a68ed57adbf7cf2b3011cdf9ce013761bd0
swerasnym <swerasnym@users.noreply.github.com>
2024-12-23 07:35:34 +0100

    2024 day 23

---
+++ src/2024/aoc2024_day23.erl
@@ -0,0 +1,73 @@
+-module(aoc2024_day23).
+-behaviour(aoc_solution).
+
+-export([run/0, run/2]).
+
+%% callbacks
+-export([info/0, star1/1, star2/1, read/1]).
+
+info() ->
+    Examples = [
+        {"examples/2024/day23_ex.txt", star1, 7},
+        {"examples/2024/day23_ex.txt", star2, "co,de,ka,ta"}
+    ],
+
+    maps:merge(aoc_solution:default_info(), #{
+        problem => {2024, 23},
+        examples => Examples
+    }).
+
+run() ->
+    aoc_solution:run(?MODULE).
+
+run(StarOrStars, FileOrData) ->
+    aoc_solution:run(?MODULE, StarOrStars, FileOrData).
+
+star1(G) ->
+    length(lists:usort(lists:flatten([tipples(A, B, G) || [$t, _] = A := AN <- G, B <- AN]))).
+
+star2(G) ->
+    CS = find_cliques(G),
+    {_, L} = lists:max(CS),
+    io:format("~p~n", [lists:sort(CS)]),
+    string:join(L, ",").
+
+read(File) ->
+    build_graph(tools:read_lines(File, fun(L) -> string:split(L, "-") end), #{}).
+
+build_graph([], G) ->
+    #{A => lists:sort(AN) || A := AN <- G};
+build_graph([[A, B] | Rest], G) ->
+    AN = maps:get(A, G, []),
+    BN = maps:get(B, G, []),
+    build_graph(Rest, G#{A => [B | AN], B => [A | BN]}).
+
+tipples(A, B, G) ->
+    AN = maps:get(A, G),
+    BN = maps:get(B, G),
+    CN = tools:overlap(AN, BN),
+    [list_to_tuple(lists:sort([A, B, C])) || C <- CN].
+
+find_cliques(G) ->
+    Nodes = maps:keys(G),
+    find_cliques(G, sets:new(), lists:sort(Nodes), sets:new(), []).
+
+find_cliques(_G, R, [], X, Result) ->
+    case sets:size(X) of
+        0 ->
+            Res = lists:sort(sets:to_list(R)),
+            [{length(Res), Res} | Result];
+        _ ->
+            Result
+    end;
+find_cliques(G, R, [V | Pr] = P, X, Result) ->
+    NV = maps:get(V, G),
+    NS = sets:from_list(NV),
+    PS = sets:from_list(P),
+
+    Res = find_cliques(
+        G, sets:add_element(V, R), sets:intersection(PS, NS), sets:intersection(X, NS), Result
+    ),
+    find_cliques(G, R, Pr, sets:add_element(V, X), Res);
+find_cliques(G, R, P, X, Result) ->
+    find_cliques(G, R, sets:to_list(P), X, Result).
