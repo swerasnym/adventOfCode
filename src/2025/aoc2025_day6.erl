398d07a3f2ec480b0a7f2b5fc8171cbabf1205fd
swerasnym <rasmus_nyman@hotmail.com>
2025-12-06 06:45:13 +0100

    2025 Day 6

--- src/2025/aoc2025_day6.erl
+++ src/2025/aoc2025_day6.erl
@@ -8,8 +8,8 @@
 
 info() ->
     Examples = [
-        {"examples/2025/day6_ex.txt", star1, unknown},
-        {"examples/2025/day6_ex.txt", star2, unknown}
+        {"examples/2025/day6_ex.txt", star1, 4277556},
+        {"examples/2025/day6_ex.txt", star2, 3263827}
     ],
 
     maps:merge(aoc_solution:default_info(), #{
@@ -23,13 +23,34 @@ run() ->
 run(StarOrStars, FileOrData) ->
     aoc_solution:run(?MODULE, StarOrStars, FileOrData).
 
-star1(Data) ->
-    io:format("~kp~n", [Data]),
-    unknown.
+star1(String) ->
+    [Ops| ValuesS] = lists:reverse(tools:parse_lines(String, fun(L) -> string:tokens(L, " ") end)),
+    Values = lists:map(fun (L) -> [list_to_integer(I)|| I <- L] end, ValuesS),
 
-star2(Data) ->
-    Data,
-    unknown.
+    G = tools:lists_to_grid(Values),
+    Gr = tools:rotate_grid(G, cw),
+    ValuesR = tools:grid_to_lists(Gr),
+
+    solve_column(Ops, ValuesR, 0).
+
+star2(String) ->
+    [ValuesS, OpsS] = string:split(String, "\n", trailing),
+    Grid = tools:parse_grid(ValuesS),
+    GridR = tools:rotate_grid(Grid, ccw),
+    % tools:print_grid(GridR),
+
+    Ops = string:tokens(lists:reverse(OpsS)," "),
+    Input = string:join(tools:grid_to_lists(GridR), "\n"),
+    Values = tools:parse_blocks(string:replace(Input, " ", "", all), fun tools:parse_integers/1),
+
+    solve_column(Ops, Values, 0).
 
 read(File) ->
     tools:read_string(File).
+
+solve_column([], [], Acc) ->
+    Acc;
+solve_column(["+"|Ops], [V|Values], Acc) ->
+    solve_column(Ops, Values, Acc+ lists:sum(V));
+solve_column(["*"|Ops], [V|Values], Acc) ->
+    solve_column(Ops, Values, Acc+ tools:product(V)).
