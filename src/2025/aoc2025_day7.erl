58749250ebf5a7e14db4a14c370398441ea45769
swerasnym <rasmus_nyman@hotmail.com>
2025-12-07 06:53:26 +0100

    2025 Day 7

--- src/2025/aoc2025_day7.erl
+++ src/2025/aoc2025_day7.erl
@@ -8,8 +8,8 @@
 
 info() ->
     Examples = [
-        {"examples/2025/day7_ex.txt", star1, unknown},
-        {"examples/2025/day7_ex.txt", star2, unknown}
+        {"examples/2025/day7_ex.txt", star1, 21},
+        {"examples/2025/day7_ex.txt", star2, 40}
     ],
 
     maps:merge(aoc_solution:default_info(), #{
@@ -23,13 +23,56 @@ run() ->
 run(StarOrStars, FileOrData) ->
     aoc_solution:run(?MODULE, StarOrStars, FileOrData).
 
-star1(Data) ->
-    io:format("~kp~n", [Data]),
-    unknown.
+star1({S, Grid}) ->
+    simulate(Grid, [S], 0).
 
-star2(Data) ->
-    Data,
-    unknown.
+star2({S, Grid}) ->
+    simulate2(Grid, [S], #{S => 1}).
 
 read(File) ->
-    tools:read_string(File).
+    Grid = tools:read_grid(File),
+    [Start] = [S || S := $S <- Grid],
+    {Start, Grid}.
+
+down({X, Y}) -> {X, Y + 1}.
+
+split({X, Y}) -> [{X - 1, Y}, {X + 1, Y}].
+
+simulate(Grid, [Pos | Rest], Splits) ->
+    Down = down(Pos),
+    case maps:get(Down, Grid, bottom) of
+        bottom ->
+            tools:print_grid(Grid),
+            Splits;
+        $| ->
+            simulate(Grid, Rest, Splits);
+        $. ->
+            simulate(Grid#{Down => $|}, Rest ++ [Down], Splits);
+        $^ ->
+            Split = [L, R] = split(Down),
+            simulate(Grid#{L => $|, R => $|}, Rest ++ Split, Splits + 1)
+    end.
+
+simulate2(Grid, [Pos | Rest], Counts) ->
+    Down = down(Pos),
+    Beams = maps:get(Pos, Counts),
+    Split = [L, R] = split(Down),
+    case maps:get(Down, Grid, bottom) of
+        bottom ->
+            lists:sum([maps:get(P, Counts) || P <- [Pos | Rest]]);
+        $| ->
+            BeamsOut = Beams + maps:get(Down, Counts),
+            case [maps:get(L, Grid), maps:get(R, Grid)] of
+                [$^, _] -> simulate2(Grid, Rest, Counts#{Down => BeamsOut});
+                [_, $^] -> simulate2(Grid, Rest, Counts#{Down => BeamsOut});
+                [_, _] -> simulate2(Grid, Rest, Counts#{Down => Beams})
+            end;
+        $. ->
+            simulate2(Grid#{Down => $|}, Rest ++ [Down], Counts#{Down => Beams});
+        $^ ->
+            BeamsOutL = Beams + maps:get(L, Counts, 0),
+            BeamsOutR = Beams + maps:get(R, Counts, 0),
+            simulate2(Grid#{L => $|, R => $|}, Rest ++ Split, Counts#{
+                L => BeamsOutL, R => BeamsOutR
+            })
+    end.
