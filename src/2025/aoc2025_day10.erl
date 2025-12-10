eacf3dc99aca5e5c9599cdf30fc70b29323bd810
swerasnym <swerasnym@users.noreply.github.com>
2025-12-10 06:44:41 +0100

    Part 1

--- src/2025/aoc2025_day10.erl
+++ src/2025/aoc2025_day10.erl
@@ -8,7 +8,7 @@
 
 info() ->
     Examples = [
-        {"examples/2025/day10_ex.txt", star1, unknown},
+        {"examples/2025/day10_ex.txt", star1, 7},
         {"examples/2025/day10_ex.txt", star2, unknown}
     ],
 
@@ -23,13 +23,48 @@ run() ->
 run(StarOrStars, FileOrData) ->
     aoc_solution:run(?MODULE, StarOrStars, FileOrData).
 
-star1(Data) ->
-    io:format("~kp~n", [Data]),
-    unknown.
+star1(Machines) ->
+    Solve = [min_presses(Wanted, Buttons) || {Wanted, Buttons, _} <- Machines],
+
+    lists:sum(Solve).
 
 star2(Data) ->
     Data,
     unknown.
 
 read(File) ->
-    tools:read_string(File).
+    tools:read_lines(File, fun parse_line/1).
+
+parse_line(Line) ->
+    [Wanted | ButtonsPowerS] = string:tokens(Line, "[] (){}"),
+    ButtonsPower = [tools:parse_integers(T, ",") || T <- ButtonsPowerS],
+    Power = lists:last(ButtonsPower),
+    Buttons = lists:droplast(ButtonsPower),
+    {Wanted, Buttons, Power}.
+
+min_presses(Wanted, Buttons) ->
+    io:format("~s, ~kp~n", [Wanted, Buttons]),
+    Enumerated = lists:enumerate(0, Wanted),
+    Start = #{I => $. || {I, _} <- Enumerated},
+    End = #{I => S || {I, S} <- Enumerated},
+    {value, P} = min_presses(Start, End, Buttons, 0),
+    P.
+
+min_presses(End, End, _, P) ->
+    {value, P};
+min_presses(_, _, [], _) ->
+    false;
+min_presses(State, End, [Button | Rest], P) ->
+    A = min_presses(press(State, Button), End, Rest, P + 1),
+    B = min_presses(State, End, Rest, P),
+    case {A, B} of
+        {false, Rb} -> Rb;
+        {Ra, false} -> Ra;
+        {{value, Ra}, {value, Rb}} -> {value, min(Ra, Rb)}
+    end.
+
+press(State, Button) ->
+    maps:merge(State, #{I => toggle(maps:get(I, State)) || I <- Button}).
+
+toggle($.) -> $#;
+toggle($#) -> $..
