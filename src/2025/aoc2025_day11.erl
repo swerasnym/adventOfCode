86d9167a3dfaa782d012c3a1ea9dddd5148039ce
swerasnym <swerasnym@users.noreply.github.com>
2025-12-11 00:01:39 +0100

    baseline

---
+++ src/2025/aoc2025_day11.erl
@@ -0,0 +1,35 @@
+-module(aoc2025_day11).
+-behaviour(aoc_solution).
+
+-export([run/0, run/2]).
+
+%% callbacks
+-export([info/0, star1/1, star2/1, read/1]).
+
+info() ->
+    Examples = [
+        {"examples/2025/day11_ex.txt", star1, unknown},
+        {"examples/2025/day11_ex.txt", star2, unknown}
+    ],
+
+    maps:merge(aoc_solution:default_info(), #{
+        problem => {2025, 11},
+        examples => Examples
+    }).
+
+run() ->
+    aoc_solution:run(?MODULE).
+
+run(StarOrStars, FileOrData) ->
+    aoc_solution:run(?MODULE, StarOrStars, FileOrData).
+
+star1(Data) ->
+    io:format("~kp~n", [Data]),
+    unknown.
+
+star2(Data) ->
+    Data,
+    unknown.
+
+read(File) ->
+    tools:read_string(File).
