97be9a0e382c642edba9fdaab09a1a8428c3905d
swerasnym <swerasnym@users.noreply.github.com>
2025-12-12 07:01:27 +0100

    2025 Solved

--- src/2025/aoc2025_day12.erl
+++ src/2025/aoc2025_day12.erl
@@ -8,8 +8,7 @@
 
 info() ->
     Examples = [
-        {"examples/2025/day12_ex.txt", star1, unknown},
-        {"examples/2025/day12_ex.txt", star2, unknown}
+        {"examples/2025/day12_ex.txt", star1, 0}
     ],
 
     maps:merge(aoc_solution:default_info(), #{
@@ -23,13 +22,46 @@ run() ->
 run(StarOrStars, FileOrData) ->
     aoc_solution:run(?MODULE, StarOrStars, FileOrData).
 
-star1(Data) ->
-    io:format("~kp~n", [Data]),
-    unknown.
+star1({_Packets, Areas}) ->
+    {Trivial, _NonTrivial} = lists:partition(fun trivial/1, Areas),
+    length(Trivial).
 
-star2(Data) ->
-    Data,
-    unknown.
+star2(_) ->
+    {done, "Done Decorating"}.
 
 read(File) ->
-    tools:read_string(File).
+    [Packets, Regions] = string:split(tools:read_string(File), "\n\n", trailing),
+
+    {parse_packets(Packets), tools:parse_lines(Regions, fun parse_region/1)}.
+
+parse_packets(Packets) ->
+    maps:from_list(tools:parse_blocks(Packets, fun parse_packet/1)).
+
+parse_packet(Packet) ->
+    [DigitS, GridS] = string:split(Packet, ":\n"),
+    Grid = tools:parse_grid(GridS),
+    NoEmpty = maps:filter(fun(K, V) -> V == $# orelse K == max end, Grid),
+
+    {list_to_integer(DigitS), all_shapes(NoEmpty)}.
+
+parse_region(Region) ->
+    [Shape, NoPacketsS] = string:split(Region, ": "),
+    [X, Y] = tools:parse_integers(Shape, "x"),
+    NoPackets = tools:parse_integers(NoPacketsS, " "),
+    {{X, Y}, lists:sum(NoPackets), lists:enumerate(0, NoPackets)}.
+
+all_shapes(Packet) ->
+    Flip = tools:flip_grid(Packet),
+    lists:usort([
+        Packet,
+        tools:rotate_grid(Packet, cw),
+        tools:rotate_grid(Packet, ccw),
+        tools:rotate_grid(tools:rotate_grid(Packet)),
+        Flip,
+        tools:rotate_grid(Flip, cw),
+        tools:rotate_grid(Flip, ccw),
+        tools:rotate_grid(tools:rotate_grid(Flip))
+    ]).
+
+trivial({{W, H}, S, _}) ->
+    (W div 3) * (H div 3) >= S.
